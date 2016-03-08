508160a289328a6f580690724c719e3809b3014e
TERRENCE WHITTAKER <terr.whittakerjr@gmail.com>
2016-03-08 08:35:19 -0500

    first commit

---
+++ app/assets/javascripts/destinations.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	function initialize() {
+		var centerPoint = new google.maps.LatLng(latitude, longitude);
+		
+		var mapOptions = {
+			center: centerPoint,
+			zoom: 17,
+			scrollwheel: false
+		}
+		var map = new google.maps.Map(document.getElementById('destination-map'), mapOptions);
+
+		var marker = new google.maps.Marker({
+			position: centerPoint,
+			map: map,
+			animation: google.maps.Animation.DROP
+		})
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
+});
